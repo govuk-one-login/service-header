9dc06f22a564ad55198b8bd013914f7942c10da1
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2023-06-29 09:08:07 +0100

    GUA-899: Optimisations for prototype kit/npm
    Implement configuration to make it easier for users to install the
    header in their Prototype Kit projects. This configuration specifies
    load paths for the SCSS and JS as well as Nunjucks paths making it so
    that the Nunjucks template for the header can be included easily into
    any Prototype Kit project.
    
    There is an additional step to have the header added as a plugin to the
    Prototype Kit UI which would allow users to install it by simply
    clicking a button. For this to happen the header must be manually
    added to an allow list of plugins by someone from the Prototype Kit
    team.
    
    Organise the `dist` folder based on file type

--- build.js
+++ build.js
@@ -1,8 +1,21 @@
 const fs = require('fs');
 const nunjucks = require('nunjucks');
-
-var previewHtml = nunjucks.render("src/preview.njk");
-var headerHtml = nunjucks.render("src/service-header.njk");
+var params =  { serviceName: "Name of example service", navigationItems: [
+  {
+    href: "#",
+    text: 'service link 1',
+    id: 'servicelink1'
+  },{
+    href: "#",
+    text: 'service link 2',
+    id: 'servicelink2'
+  },{
+    href: "#",
+    text: 'service link 3',
+    id: 'servicelink3'
+  }] }
+const previewHtml = nunjucks.render("src/preview.njk");
+const headerHtml = nunjucks.render("src/template.njk", {params: params});
 
 fs.writeFile(__dirname + '/dist/preview.html', previewHtml, err => {
   if (err) {
@@ -11,19 +24,35 @@ fs.writeFile(__dirname + '/dist/preview.html', previewHtml, err => {
   console.log('header preview generated');
 });
 
-fs.writeFile(__dirname + '/dist/header.html', headerHtml, err => {
+fs.writeFile(__dirname + '/dist/html/header.html', headerHtml, err => {
   if (err) {
     console.error(err);
   }
-  console.log('header copied to dist folder');
+  console.log('header HTML copied to dist folder');
 });
 
-fs.copyFile("src/service-header.js", __dirname + '/dist/service-header.js', (err) => {
+fs.copyFile("src/service-header.njk", __dirname + '/dist/nunjucks/service-header.njk', (err) => {
+  if (err) throw err;
+  console.log('header nunjucks macro copied to dist folder');
+});
+
+fs.copyFile("src/template.njk", __dirname + '/dist/nunjucks/template.njk', (err) => {
+  if (err) throw err;
+  console.log('header nunjucks template copied to dist folder');
+});
+
+
+fs.copyFile("src/service-header.js", __dirname + '/dist/scripts/service-header.js', (err) => {
   if (err) throw err;
   console.log('service-header.js copied to dist folder');
 });
 
-fs.copyFile("src/service-header.scss", __dirname + '/dist/service-header.scss', (err) => {
+fs.copyFile("src/service-header.scss", __dirname + '/dist/styles/service-header.scss', (err) => {
   if (err) throw err;
   console.log('service-header.scss file copied to dist folder');
+});
+
+fs.copyFile("src/service-header-no-imports.scss", __dirname + '/dist/styles/service-header-no-imports.scss', (err) => {
+  if (err) throw err;
+  console.log('service-header-no-imports.scss file copied to dist folder');
 });
